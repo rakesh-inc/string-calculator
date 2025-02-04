fd152632c6409954db36e6801f0ee7253f79724d
Rakesh Kumar <rakesh@incubyte.co>
2025-02-04 17:09:10 +0530

    refactor: implement beforeEach and remove redundant code

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,13 +1,16 @@
 import { StringCalculator } from "./string-calculator";
 
 describe("StringCalculator", () => {
+  let calculator: StringCalculator;
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
   it("should return 0 when empty string is passed", () => {
-    let calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
   });
 
   it("should return number when a single number number is passed", () => {
-    let calculator = new StringCalculator();
     expect(calculator.add("4")).toBe(4);
   });
 });
